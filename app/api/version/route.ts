8d87924c76fc5796694fd36822bcf98a21ef9fe9
Oodri <info@oodri.dev>
2025-01-21 18:21:38 +0200

    ⭐version 2

--- app/api/version/route.ts
+++ app/api/version/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  return new Response(JSON.stringify({ version: 1 }), {
+  return new Response(JSON.stringify({ version: 2 }), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
